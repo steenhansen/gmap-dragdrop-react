113ae4a81a63c7101cffdef71fd5d58604e56251
steen hansen <steen_hansen@yahoo.com>
2017-06-28 20:28:10 -0700

    gulp 4

--- lint-component.js
+++ lint-component.js
@@ -1,37 +1,26 @@
 'use strict'
 
-// gulp lint-component-once
-// gulp lint-component-repeat
-
-process.chdir(__dirname)
+// gulp lint-component
 
 var gulp = require('gulp')
 var eslint = require('gulp-eslint')
 var gutil = require('gulp-util')
 const debug = require('gulp-debug')
-
 var start_text_color = 'bgRed'
-var stop_text_color = 'bgGreen'
 
 var my_lint_dirs = [
-  './jsx/*.jsx'
-  , './*.js'
+    __dirname + '/jsx/*.jsx'
+  , __dirname + '/*.js'
 ]
 var lint_text = '                                    component lint'
 
-gulp.task('COMP_LINT_1', function () {
+gulp.task('lint-component', function (cb) {
   console.log(gutil.colors[start_text_color](lint_text))
    gulp.src(my_lint_dirs)
           .pipe(eslint())
           .pipe(eslint.format())
-          .pipe(debug({title: 'LINT_component_check:'}))
+          .pipe(debug({title: 'lint_COMPONENT_check:'}))
+  cb()
 })
 
-gulp.task('lint-component-once', ['COMP_LINT_1'], function () {
-  console.log(gutil.colors[stop_text_color](lint_text))
-})
-
-gulp.task('lint-component-repeat', ['lint-component-once'], function () {
-  gulp.watch(my_lint_dirs, ['lint-component-once'])
-})
 
